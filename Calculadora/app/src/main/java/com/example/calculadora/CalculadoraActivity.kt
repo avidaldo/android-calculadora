f3364b9a2b3de3eb8a16344c891b0269f4318e49
avidaldo <avd.formacion@gmail.com>
2021-11-03 18:17:55 +0100

    Interfaz completo, con temas y efecto ola en botones

--- Calculadora/app/src/main/java/com/example/calculadora/CalculadoraActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/CalculadoraActivity.kt
@@ -2,6 +2,7 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.calculadora.databinding.ActivityCalculadoraBinding
 
 class CalculadoraActivity : AppCompatActivity() {
@@ -12,6 +13,40 @@ class CalculadoraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculadoraBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.tv11Percent.setOnClickListener { }
+        binding.tv12CE.setOnClickListener { }
+        binding.tv13C.setOnClickListener { }
+        binding.tv14EraseLeft.setOnClickListener { }
+        binding.tv21Inverso.setOnClickListener { }
+        binding.tv22Cuadrado.setOnClickListener { }
+        binding.tv23Raiz.setOnClickListener { }
+        binding.tv24opDividir.setOnClickListener { }
+        binding.tv31num7.setOnClickListener { }
+        binding.tv32num8.setOnClickListener { }
+        binding.tv33num9.setOnClickListener { }
+        binding.tv34opMultiplica.setOnClickListener { }
+        binding.tv41num4.setOnClickListener { }
+        binding.tv42num5.setOnClickListener { }
+        binding.tv43num6.setOnClickListener { }
+        binding.tv44opResta.setOnClickListener { }
+        binding.tv51num1.setOnClickListener { }
+        binding.tv52num2.setOnClickListener { }
+        binding.tv53num3.setOnClickListener { }
+        binding.tv54opSuma.setOnClickListener { }
+        binding.tv61negate.setOnClickListener { }
+        binding.tv62num0.setOnClickListener { }
+        binding.tv63decimal.setOnClickListener { }
+        binding.tv64opIgual.setOnClickListener { }
+
+
+        binding.tvMC.setOnClickListener { }
+        binding.tvMR.setOnClickListener { }
+        binding.tvMsum.setOnClickListener { }
+        binding.tvMrestar.setOnClickListener { }
+        binding.MS.setOnClickListener { }
+        binding.tvMHistorial.setOnClickListener { }
 
 
     }
--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 ## Ejemplo de calculadora en Android
-
-Por ahora solo cuenta con el interfaz gráfico, hecho con un solo ConstraintLayout, sin anidar otros layouts y utilizando guidelines y styles
-
-
-
-
-https://developer.android.com/guide/topics/ui/look-and-feel/themes
